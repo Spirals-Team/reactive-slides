9fea2acb767f148e7ce954c3d6ade6f5a30d7414
rbadr <rahal.badr@gmail.com>
2017-02-10 13:43:34 +0100

    Update questions views

--- examples/app/views/Question1View.scala
+++ examples/app/views/Question1View.scala
@@ -5,10 +5,10 @@ object Question1View {
   import dsl.Dsl._
   import scalatags.Text.all._
 
-  def apply(title: String, theme: String) = {
-    presentation(title, theme,
+  def apply(title: String, description: String, author: String, theme: String) = {
+    presentation(title, description, author, theme,
       slide(
-        survey("Question1", "Are you working ?","Yes", "No")
+        survey("Question1", 2, "Are You Paying Attention ?","Yes", "No")
       )
     )
   }
--- examples/app/views/Question2View.scala
+++ examples/app/views/Question2View.scala
@@ -5,10 +5,10 @@ object Question2View {
   import dsl.Dsl._
   import scalatags.Text.all._
 
-  def apply(title: String, theme: String) = {
-    presentation(title, theme,
+  def apply(title: String, description: String, author: String, theme: String) = {
+    presentation(title, description, author, theme,
       slide(
-        survey("Question2", "Which device ?","Smartphone", "Tablet", "Laptop")
+        survey("Question2", 3, "Which device are you using ?","Smartphone", "Tablet", "Laptop")
       )
     )
   }
